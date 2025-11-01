c781e4ae592694849807b1bbd4768142c4a88ac3
Zarpoosh <mzar67839@gmail.com>
2025-11-01 22:29:01 +0330

    remove button import

--- src/components/ slider/InageSlider.tsx
+++ src/components/ slider/InageSlider.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./InageSlider.css";
 
