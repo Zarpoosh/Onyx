d6f84ff353280c1f9a8a68414cdce56514591c6b
Zarpoosh <mzar67839@gmail.com>
2025-11-01 22:26:53 +0330

    add industrialcrick page: slider & some products

--- src/components/ slider/InageSlider.tsx
+++ src/components/ slider/InageSlider.tsx
@@ -1,66 +1,57 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './InageSlider.css';
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./InageSlider.css";
 
-const InageSlider: React.FC = () => {
-  const kaolinSlides = [
-    "https://onyxcompany.ir/assets/IMG_2287-BM3eospK.jpeg",
-    "https://onyxcompany.ir/assets/NN22-D46a2zYr.jpeg",
-    "https://onyxcompany.ir/assets/top-2-mz1dh84G.jpeg",
+const ImageSlider: React.FC = () => {
+  const slides = [
+    "/public/images/img3.jpg",
+    "/public/images/crick.jpeg",
+    "/public/images/kureh.jpeg",
   ];
 
-  const nesoozSanatiSlides = [
-    "https://onyxcompany.ir/assets/IMG_6523-D5GpIDHD.png",
-    "https://onyxcompany.ir/assets/kore-KdTZM4wp.jpeg"  
+  const industries = [
+    "فولاد",
+    "ریخته گری",
+    "سیمان",
+    "پتروشیمی",
+    "شیشه و سرامیک",
   ];
+  return (
+    <div className="image-slider-wrapper position-relative">
+      <Carousel fade interval={4000} controls indicators>
+        {slides.map((src, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-100 slider-img"
+              src={src}
+              alt={`Slide ${index + 1}`}
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
 
-  const nesoozNamaSlides = [
-     "/images/img1.jpg",
-    "/images/img2.jpg",
-    "/images/img3.jpg",
-  ];
+      {/* متن ثابت روی همه اسلایدها */}
+      <div className="carousel-fixed-text position-absolute top-50 start-50 translate-middle text-center text-white px-3">
 
-  const sefalSlides = [
-    "https://images.pexels.com/photos/280471/pexels-photo-280471.jpeg",
-    "https://images.pexels.com/photos/37347/tools-workshop-equipment-tools.jpg",
-    "https://images.pexels.com/photos/723240/pexels-photo-723240.jpeg",
-  ];
-
-  const renderSlider = (title: string, slides: string[]) => (
-    <div className="mb-5">
-      <h2 className="text-center fw-bold mb-4">{title}</h2>
-      <div className="image-slider-wrapper position-relative">
-        <Carousel fade interval={4000} controls indicators>
-          {slides.map((src, index) => (
-            <Carousel.Item key={index}>
-              <img
-                className="d-block w-100 slider-img"
-                src={src}
-                alt={`${title} Slide ${index + 1}`}
-              />
-            </Carousel.Item>
-          ))}
-        </Carousel>
-
-        <div className="carousel-fixed-text position-absolute top-50 start-50 translate-middle text-center text-white px-3">
-          <h3>{title}</h3>
-          <Button variant="btn-custom" className='btn-custom fw-bold m-1' href="#read-more">بیشتر</Button>
-          <Button variant="dark" className=' bg-dark' href="#read-more">خدمات</Button>
+        <div className="industries-list mt-4 text-center">
+          <h3 className="mb-4 ">صنایع مصرف کننده</h3>
+          <div className="industries-container rtl-direction">
+            <ul className="list-unstyled d-flex flex-wrap justify-content-center gap-3">
+              {industries.map((industry, index) => (
+                <li key={index} className="industry-item m-1 text-dark">
+                  <span className="industry-badge bg-light bg-opacity-75 rounded px-3 py-2">
+                    {industry}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
   );
-
-  return (
-    <div id="home" className="container-fluid my-5">
-      {renderSlider("کائولن", kaolinSlides)}
-      {renderSlider("نسوز صنعتی", nesoozSanatiSlides)}
-      {renderSlider("نسوزنما", nesoozNamaSlides)}
-      {renderSlider("سفال", sefalSlides)}
-    </div>
-  );
 };
 
-export default InageSlider;
+export default ImageSlider;
--- src/components/productdetails/IndustrialBricksDetail.tsx
+++ src/components/productdetails/IndustrialBricksDetail.tsx
@@ -1,12 +1,57 @@
-const KaolinDetail = () => (
-  <div className="container py-5 min-vh-100 d-flex align-items-center">
-    <div className="row align-items-center text-center text-md-start w-100">
-     <h1>
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import InageSlider from "../ slider/InageSlider";
+import "./IndustrialBricksDetail.css";    
 
-    این صفحه در دست ساخت است  :)
-     </h1>
-    </div>
+const items = [
+  { src: "/images/irone.jpeg", title: "صنایع فولاد و آهن" },
+  { src: "/images/Casting.jpeg", title: "ریخته گری" },
+  { src: "/images/Cement.jpeg", title: "سیمان" },
+  { src: "/images/petrochimical.jpeg", title: "پتروشیمی" },
+  { src: "/images/Glass making.jpeg", title: "شیشه و سرامیک" },
+  { src: "/images/Glucose.jpeg", title: "کوره قند و آهک" },
+];
+
+const IndustrialBricksDetail = () => (
+  <div>
+    <InageSlider />
+    <Container className="py-4 d-flex justify-content-center align-items-center">
+      <div className="text-center w-100">
+        <Row className="justify-content-center g-4">
+          {items.map((item, index) => (
+            <Col
+              key={index}
+              xs={10}
+              sm={6}
+              md={4}
+              lg={2}
+              className="d-flex flex-column align-items-center"
+            >
+              <div className="image-card-wrapper position-relative">
+                <div className="lazy-img-wrapper position-relative overflow-hidden rounded shadow-sm">
+                  <LazyLoadImage
+                    effect="blur"
+                    src={item.src}
+                    alt={item.title}
+                    className="lazy-img img-fluid"
+                  />
+                  {/* متن روی تصویر */}
+                  {/* متن روی تصویر */}
+                  <div className="image-overlay position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center">
+                    <div className="overlay-content text-white text-center p-3">
+                      <h6 className="overlay-title mb-0">{item.title}</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </Container>
   </div>
 );
 
-export default KaolinDetail;
+export default IndustrialBricksDetail;
